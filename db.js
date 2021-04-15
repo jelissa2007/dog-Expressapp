0ba783d4ab0c1f90069758686529d0f09e7d2af4
Eric Treadway <eric.s.treadway@gmail.com>
2021-04-15 18:35:13 -0500

    first commit

---
+++ db.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        name: 'Shane',
+        breed: 'Golden-mix',
+        image: 
+
+    }
+]
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const server = http.createServer(app);
+const db = require('./db')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
